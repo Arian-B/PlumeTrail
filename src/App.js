5caa87257f691ab31638f8da6bdc8b80ee980624
Arian-B <arian.zg2003@gmail.com>
2025-03-31 02:01:56 +0530

    Setup of plumetrail frontend react app, nodejs and tailwind css, ficed eerrors and bugs and tested it

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className = 'text-red-500' text-5x1> hello world</h1>
+    </div>
+  );
+}
+
+export default App;
