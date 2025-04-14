07b35b229d153a605787df4ec3f41fc8bfad1d8e
Arian-B <arian.zg2003@gmail.com>
2025-04-14 22:15:51 +0530

    Fixed db.js

--- backend/db.js
+++ backend/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
   host:"localhost",
   user:"root",
-  password: process.env.DB_KEY,
-  database:"blog"
+  password: "34783478",
+  database:"plumetrail"
 })
